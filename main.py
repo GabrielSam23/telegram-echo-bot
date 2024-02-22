41ce3aa8b3e7b2f73bf53077ad76356344589f05
GabrielSam23 <143095210+GabrielSam23@users.noreply.github.com>
2024-02-22 08:38:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import discord
 from discord.ext import commands
-from discord_components import DiscordComponents, Select, SelectOption
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_components import create_select, create_select_option, wait_for_component
 from keep_alive import keep_alive
 
 keep_alive()
@@ -31,8 +32,7 @@ form_message_ids = {}
 # Inicialização do bot
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
-# Inicialização do Discord Components
-DiscordComponents(bot)
+slash = SlashCommand(bot, sync_commands=True)
 
 
 @bot.event
@@ -82,14 +82,15 @@ async def enviarformulario(ctx):
             name=field, value="Digite sua resposta aqui", inline=False)
 
     # Adicionando campo de disponibilidade com menu suspenso
-    select = Select(
-        placeholder="Selecione sua disponibilidade",
+    select = create_select(
         options=[
-            SelectOption(label="Manhã", value="Manhã"),
-            SelectOption(label="Tarde", value="Tarde"),
-            SelectOption(label="Noite", value="Noite"),
-            SelectOption(label="Todas as opções", value="Todas as opções")
-        ]
+            create_select_option("Manhã", "manha"),
+            create_select_option("Tarde", "tarde"),
+            create_select_option("Noite", "noite"),
+            create_select_option("Todas as opções", "todas")
+        ],
+        placeholder="Selecione sua disponibilidade",
+        custom_id="disponibilidade"
     )
 
     message = await ctx.send(embed=embed, components=[select])
@@ -98,8 +99,8 @@ async def enviarformulario(ctx):
     respostas = {}
 
     # Aguarda a seleção da disponibilidade
-    interaction = await bot.wait_for("select_option", check=lambda i: i.component == select)
-    disponibilidade = interaction.values[0]
+    interaction = await wait_for_component(bot, components=select)
+    disponibilidade = interaction.selected_options[0]
     respostas["Disponibilidade"] = disponibilidade
 
     # Atualiza o embed com a resposta de disponibilidade
